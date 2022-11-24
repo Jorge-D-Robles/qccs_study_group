8ce571f768f1921750044fa6aa3ff12520ba4087
Jorge Robles <robles8925@gmail.com>
2022-11-24 13:22:41 -0500

    recursion

---
+++ fall2022/recursion/help.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 0; i < 100; i++)
+    {
+        cout << "SOS send help times " << i + 1 << endl;
+    }
+}
--- fall2022/recursion/recursion5.cpp
+++ fall2022/recursion/recursion5.cpp
@@ -9,7 +9,7 @@ int main()
 }
 string baseChange(int n, int base)
 {
-    
+
 }
 
 // which converts the decimal number n to the given base.
@@ -17,3 +17,18 @@ string baseChange(int n, int base)
 //  baseChange(5,156)
 // would return 1111, because 156 is 125 + 25 + 5 + 1 which is
 // written as 1111 in base 5.
+
+int arr[5] = {1, 2, 3, 4, 5}
+
+int function(int arr[], int x)
+{
+    string fred = "hey its fred";
+    fredAge = arr[4];
+    return fred;
+}
+
+function(int *arr)
+
+in main: 
+
+student = function(arr, x)
